30a50cc0e50815daccdc91a0945c7eab254de3e2
nickfff-dev <hunchographics@gmail.com>
2022-03-29 00:42:08 +0300

    s

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
 
 
-fetch("https://rest.bandsintown.com/artists/dababy/events/?app_id=")
+fetch("https://rest.bandsintown.com/artists/lilwayne/events/?app_id=2b86392a65721cdf3698bf18d6bfba28")
     .then(function (response) {
         return response.json()
     
@@ -12,14 +12,17 @@ fetch("https://rest.bandsintown.com/artists/dababy/events/?app_id=")
         var eventsModal = document.querySelector(".view-content")
 
         for (var i = 0; i < events.length; i++) {
-            var event = events[i]
+          var event = events[i]
+          var name = event.artist["name"]
+          
             var day = event.datetime.slice(8, 10)
             var month = event.datetime.slice(5, 7)
             var city = event.venue.city
             var country = event.venue.country
             var name = event.venue.name
             var ticketurl = event.offers[0].url
-            var eventDiv = `<div class="ds-3col-equal node node--event views-row views-row-17 views-row-odd view-mode-summary  node--summary node--event--summary clearfix bolero-analytics-processed">
+          var eventDiv = ` <h1>${name}</h1>
+            <div class="ds-3col-equal node node--event views-row views-row-17 views-row-odd view-mode-summary  node--summary node--event--summary clearfix bolero-analytics-processed">
 
             <div class="group-left ds-region ">
               <div class="field field--name-asf-events-small-date field--type-ds field--label-hidden stacked">
@@ -30,7 +33,7 @@ fetch("https://rest.bandsintown.com/artists/dababy/events/?app_id=")
             </div>
             
             <div class="group-middle ds-region ">
-              <div class="field field--name-asf-events-city field--type-ds field--label-hidden ">
+              <div class="field field--name-asf-events-city field--type-ds field--label-hidden">
                 <div class="field__items">
                   <div class="field__item"><a><span class="locality">${city},</span>&nbsp;<span class="area">${country}</span></a></div>
                 </div>
