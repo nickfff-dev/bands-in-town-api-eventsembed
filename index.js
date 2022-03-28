0d6915d34683475b783be52cb912b28918280ce1
nickfff-dev <hunchographics@gmail.com>
2022-03-29 00:29:49 +0300

    bands intown

---
+++ index.js
@@ -0,0 +1,59 @@
+
+
+
+fetch("https://rest.bandsintown.com/artists/dababy/events/?app_id=2b86392a65721cdf3698bf18d6bfba28")
+    .then(function (response) {
+        return response.json()
+    
+    }).then(function (json) {
+        console.log(json)
+        
+        var events = json
+        var kitu = document.getElementById("sufuria")
+
+        for (var i = 0; i < events.length; i++) {
+            var event = events[i]
+            var day = event.datetime.slice(8, 10)
+            var month = event.datetime.slice(5, 7)
+            var city = event.venue.city
+            var country = event.venue.country
+            var name = event.venue.name
+            var ticketurl = event.offers[0].url
+            var eventDiv = `<div class="ds-3col-equal node node--event views-row views-row-17 views-row-odd view-mode-summary  node--summary node--event--summary clearfix bolero-analytics-processed">
+
+            <div class="group-left ds-region ">
+              <div class="field field--name-asf-events-small-date field--type-ds field--label-hidden stacked">
+                <div class="field__items">
+                  <div class="field__item"><span class="month">${month}</span><span  class="day">${day}</span></div>
+                </div>
+              </div>
+            </div>
+            
+            <div class="group-middle ds-region ">
+              <div class="field field--name-asf-events-city field--type-ds field--label-hidden ">
+                <div class="field__items">
+                  <div class="field__item"><a><span class="locality">${city},</span>&nbsp;<span class="area">${country}</span></a></div>
+                </div>
+              </div>
+              <div class="field field--name-asf-events-venue-title field--type-ds field--label-hidden ">
+                <div class="field__items">
+                  <div class="field__item"><a class="customer">${name}</a></div>
+                </div>
+              </div>
+              <div class="field field--name-asf-events-tickets field--type-ds field--label-hidden">
+                <div class="field__items">
+                  <div class="field__item"><a href=${ticketurl} target="_blank" class="button tickets">Tickets</a></div>
+                </div>
+              </div>
+            </div>
+            
+            <div class="group-right ds-region ">
+            </div>
+            
+            </div>`
+            kitu.innerHTML += eventDiv
+
+           
+        }
+    })
+    
